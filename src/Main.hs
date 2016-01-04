5faa1f3a96b178a1d3ed8ecd2f7d1db73a2afd5e
David Pedersen <david.pdrsn@gmail.com>
2016-01-04 13:34:52 +0100

    Better separation of pure and impure code

--- src/Main.hs
+++ src/Main.hs
@@ -9,6 +9,8 @@ import Network.Wai.Handler.Warp
 import System.Directory
 import System.FilePath.Posix
 import App
+import Data.Text.Lazy (Text)
+import Control.Monad
 
 import qualified Data.Map as M
 import qualified Data.Text.Lazy.IO as T
@@ -24,13 +26,22 @@ port = 4000
 
 compileAllPostsInDir :: FilePath -> IO Posts
 compileAllPostsInDir dir = do
-    let dir' = dir ++ "/"
-    markdownFilePaths <- filter isMarkdownFile <$> map (dir' ++) <$> getDirectoryContents dir
-    contents <- map compileMarkdown <$> mapM T.readFile markdownFilePaths
-    let
-      markdownFileNames = map (cs . takeBaseName) markdownFilePaths
-      posts = M.fromList $ zip markdownFileNames contents
-    return posts
+    filesInDir <- getFilesInDir dir
+    contents <- mapM T.readFile filesInDir
+    return $ compileMarkdownPosts $ zip filesInDir contents
+
+getFilesInDir :: FilePath -> IO [FilePath]
+getFilesInDir dir = do
+    allFiles <- map ((dir ++ "/") ++) <$> getDirectoryContents dir
+    filterM doesFileExist allFiles
+
+compileMarkdownPosts :: [(FilePath, Text)] -> Posts
+compileMarkdownPosts filesAndContents =
+    let markdownFiles = filter (isMarkdownFile . fst) filesAndContents
+        (filenames, contents) = unzip markdownFiles
+        postTitles = map (cs . takeBaseName) filenames
+        compiledContents = map compileMarkdown contents
+    in M.fromList $ zip postTitles compiledContents
 
 isMarkdownFile :: FilePath -> Bool
 isMarkdownFile = (`elem` markdownExtensions) . takeExtension
